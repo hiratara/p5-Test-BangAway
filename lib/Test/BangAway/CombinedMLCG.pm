ba86120a0bddd5c8451ed7d0d252089305b16f22
Masahiro Honma <hiratara@cpan.org>
2013-04-28 11:56:24 +0900

    Implement conbined MLCG.
    This is the Portable Combined Generator of L'Ecuyer.

---
+++ lib/Test/BangAway/CombinedMLCG.pm
@@ -0,0 +1,66 @@
+package Test::BangAway::CombinedMLCG;
+use strict;
+use warnings;
+use constant M => [2147483563, 2147483399];
+use constant A => [40014, 40692];
+use constant Q => do {
+    use integer;
+    [map { M->[$_] / A->[$_] } 0 .. $#{M()}];
+};
+use constant R => do {
+    use integer;
+    [map { M->[$_] % A->[$_] } 0 .. $#{M()}];
+};
+
+sub new {
+    my $class = shift;
+    bless [@_] => $class;
+}
+
+sub next {
+    my $self = shift;
+
+    my $z = 0;
+    for my $j (0 .. $#{M()}) {
+        use integer;
+        my $k = $self->[$j] / Q->[$j];
+        $self->[$j] = A->[$j] * ($self->[$j] - $k * Q->[$j]) - $k * R->[$j];
+        $self->[$j] += M->[$j] if $self->[$j] < 0;
+        if ($j % 2 == 0) {
+            $z += $self->[$j] - M->[0] + 1;
+        } else {
+            $z -= $self->[$j];
+        }
+        $z += M->[0] - 1 if $z < 1;
+    }
+
+    return $z;
+}
+
+sub split {
+    my $self = shift;
+
+    my $other = (ref $self)->new(@$self);
+    $other->next;
+
+    for my $j (0 .. $#{M()}) {
+        if ($j % 2 == 0) {
+            if ($self->[$j] >= M->[$j] - 1) {
+                $self->[$j] = 1;
+            } else {
+                $self->[$j] += 1;
+            }
+        } else {
+            ($self->[$j], $other->[$j]) = ($other->[$j], $self->[$j]);
+            if ($other->[$j] <= 1) {
+                $other->[$j] = M->[$j] - 1;
+            } else {
+                $other->[$j] -= 1;
+            }
+        }
+    }
+
+    return $other;
+}
+
+1;
---
+++ t/combined_mlcg.t
@@ -0,0 +1,40 @@
+use strict;
+use warnings;
+use Test::BangAway;
+use Test::BangAway::CombinedMLCG;
+use Test::BangAway::Generator;
+use Test::More;
+
+my $mlcg = Test::BangAway::CombinedMLCG->new(1, 1);
+is $mlcg->next, 2147482884;
+is $mlcg->next, 2092764894;
+my $mlcg2 = $mlcg->split;
+is $mlcg->next, 1621839889;
+is $mlcg2->next, 483997720;
+is $mlcg->next, 1388091868;
+is $mlcg2->next, 516017270;
+
+bang_away_ok {
+    my ($s1, $s2) = @_;
+    my $mlcg1 = Test::BangAway::CombinedMLCG->new($s1, $s2);
+    my $mlcg2 = Test::BangAway::CombinedMLCG->new($s1, $s2);
+    $mlcg1->next == $mlcg2->next && $mlcg1->next == $mlcg2->next;
+} concat(integer, integer);
+
+bang_away_ok {
+    my ($s1, $s2) = @_;
+    my $mlcg1 = Test::BangAway::CombinedMLCG->new($s1 + 1, $s2);
+    my $mlcg2 = Test::BangAway::CombinedMLCG->new($s1, $s2 + 1);
+    $mlcg1->next != $mlcg2->next && $mlcg1->next != $mlcg2->next;
+} concat(integer, integer);
+
+bang_away_ok {
+    my ($s1, $s2) = @_;
+    my $mlcg1 = Test::BangAway::CombinedMLCG->new($s1, $s2);
+    my $mlcg2 = $mlcg1->split;
+    my $mlcg3 = Test::BangAway::CombinedMLCG->new($s1, $s2);
+    my $mlcg4 = $mlcg3->split;
+    $mlcg1->next == $mlcg3->next && $mlcg2->next == $mlcg4->next;
+} concat(integer, integer);
+
+done_testing;
